a47829a27192372d0850ee42efb3fe4588df31fd
Pablo Valenzuela <pvalenzuela@netskope.com>
2024-10-03 15:11:37 +0200

    1.2 Push added, Pull severity choice

--- Maltiverse/main.py
+++ Maltiverse/main.py
@@ -308,7 +308,7 @@ class MaltiversePlugin(PluginBase):
 
         # Step-1
         # Convert IOCs to Maltiverse format
-        generated_payload = {}
+        generated_payload = []
         total_ioc_count = 0
         skipped_ioc = 0
 
@@ -392,7 +392,7 @@ class MaltiversePlugin(PluginBase):
                         details=str(traceback.format_exc()),
                     )
                     raise exp
-                generated_payload = {}
+                generated_payload = []
             else:
                 generated_payload.update(json.loads(ioc_payload))
 
