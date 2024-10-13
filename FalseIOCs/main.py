f5d08b7865b6e8ac8f7ae4aafc73fd74f072d394
Pablo Valenzuela <pvalenzuela@netskope.com>
2024-10-13 10:36:26 +0200

    1.0.0

--- FalseIOCs/main.py
+++ FalseIOCs/main.py
@@ -31,7 +31,7 @@ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 False IOCs Plugin.
 """
-import traceback, json
+import traceback, random, string, hashlib
 from typing import List
 
 from netskope.integrations.cte.models import Indicator, IndicatorType, SeverityType
@@ -104,139 +104,56 @@ class FalseIOCsPlugin(PluginBase):
     def pull(self) -> List[Indicator]:
         """Pull indicators from Recorded Future IOC plugin."""
         indicators = []
-        risklists = self.configuration.get("risklists", "key")
+        ioc_types = ["urls", "hosts", "md5", "sha256", "ipv4", "ipv6"]
+        indicator_count = 0
+        headers = True
 
-        self.logger.info(f"{self.log_prefix}: Pulling IOC(s) of the risklist(s) {risklists}")
-        for risklist in risklists:
-            url = ("https://api.recordedfuture.com/v2/" +
-                   risklist.lower() +
-                   "/risklist?format=application%2Fstix%2Bjson%3Bversion%3D2.1&gzip=false&list=default")
-
-            try:
-                response = self.recorded_future_ioc_helper.api_helper(
-                    url=url,
-                    method="GET",
-                    verify=self.ssl_validation,
-                    proxies=self.proxy,
-                    logger_msg="pulling IOC(s)",
-                    headers={"X-RFToken": self.configuration['apikey'],
-                             "Content-Type": "application/json",
-                             "accept": "application/json"}
-                )
-                indicators, indicator_count = self.extract_indicators(
-                    response, risklist, indicators
-                )
-
-                self.logger.debug(
-                    f"Pull Stat: {indicator_count} indicator(s) were fetched. "
-                )
-                self.logger.info(
-                    f"{self.log_prefix}: Successfully fetched "
-                    f"{indicator_count} IOC(s) "
-                    f"from the {risklist} risklist'"
-                )
-
-            except RecordedFutureIOCPluginException as exp:
-                err_msg = "Error occurred while pulling indicators."
-                self.logger.error(
-                    message=(f"{self.log_prefix}: {err_msg} Error: {str(exp)}"),
-                    details=str(traceback.format_exc()),
-                )
-                raise exp
-            except Exception as exp:
-                err_msg = "Error occurred while pulling indicators."
-                self.logger.error(
-                    message=(
-                        f"{self.log_prefix}: {err_msg} Error: {str(exp)}"
-                    ),
-                    details=str(traceback.format_exc()),
-                )
-                raise exp
+        for ioc_type in ioc_types:
+            if self.configuration.get(ioc_type, "") > 0:
+                for i in range(1,self.configuration.get(ioc_type, "")):
+                    current_type, current_value = self._get_data(self.configuration.get(ioc_type, ""))
+                    indicators.append(
+                        Indicator(
+                            value=current_value,
+                            type=current_type,
+                            severity=SeverityType.HIGH
+                        )
+                    )
 
         return indicators
 
-    def extract_indicators(self, response, risklist, indicators) -> tuple[list, int]:
+    def _get_data(self, ioc_type) -> tuple[str, str]:
         """
         Extract indicators from a given response based on the specified indicator types.
 
         Args:
-            response (str): The response from which to extract indicators.
-            indicators (string): the current IOC list
-            risklist (string): the current risklist pulled from Recoded Future
+            ioc_type (str): the type of IOC for data generation
 
         Returns:
-            Tuple[List[dict], int]: A tuple containing a list of extracted \
-                                    indicators and the number of indicators.
+            Tuple[str,str]: A tuple containing the IOC type and a value.
         """
-        indicator_count = 0
-        headers = True
+        letters = string.ascii_lowercase
+        word = ''.join(random.choice(letters) for i in range(8)) + '.com'
+        if ioc_type == "urls":
+            current_type = IndicatorType.URL
+            current_value = 'https://' + word + '/' + word
+        elif ioc_type == "hosts":
+            current_type = getattr(IndicatorType, "DOMAIN", IndicatorType.URL)
+            current_value = word
+        elif ioc_type == "md5":
+            current_type = IndicatorType.MD5
+            current_value = hashlib.md5(word.encode("utf")).hexdigest()
+        elif ioc_tyupe == "sha256":
+            current_type = IndicatorType.SHA256
+            current_value = hashlib.sha256(word.encode("utf")).hexdigest()
+        elif ioc_type == "ipv4":
+            current_type = getattr(IndicatorType, "IPV4", IndicatorType.URL)
+            current_value = ".".join(map(str, (random.randint(0, 255) for _ in range(4))))
+        else:
+            current_type = getattr(IndicatorType, "IPV6", IndicatorType.URL)
+            current_value = ':'.join('{:x}'.format(random.randint(0, 2**16 - 1)) for i in range(8))
 
-        for line in response.splitlines():
-            if not headers:
-                values = line.split('","')
-
-                # convert risklist into netskope types.
-                if risklist == 'ip':
-                    if ":" in values[0]:
-                        current_type = getattr(IndicatorType, "IPV6", IndicatorType.URL)
-                    else:
-                        current_type = getattr(IndicatorType, "IPV4", IndicatorType.URL)
-                elif risklist == 'hash':
-                    if values[1] == 'SHA-256':
-                        current_type = IndicatorType.SHA256
-                    elif values[1] == 'MD5':
-                        current_type = IndicatorType.MD5
-                    else:
-                        self.logger.debug(f"Hash type not found: {values[1]}")
-                        continue
-                elif risklist == 'domain':
-                    current_type = getattr(IndicatorType, "DOMAIN", IndicatorType.URL)
-                elif risklist == 'url':
-                    current_type = IndicatorType.URL
-                else:
-                    err_msg = "Error converting indicator type" + risklist
-                    self.logger.error(
-                        message=(
-                            f"{self.log_prefix}: {err_msg}"
-                        )
-                    )
-                    continue
-
-                current_indicator_value=values[0][1:]
-                if risklist == 'hash':
-                    current_risk_score = int(values[2])
-                    current_evidences = values[4]
-                else:
-                    current_risk_score = int(values[1])
-                    current_evidences = values[3]
-                if self.configuration.get("fetchevidences", "") == "yes":
-                    current_evidences = ''.join(current_evidences)
-                else:
-                    current_evidences = ''
-                if type(current_risk_score) is not int or current_risk_score == 0:
-                    current_risk = SeverityType.UNKNOWN
-                elif current_risk_score <= 39:
-                    current_risk = SeverityType.LOW
-                elif current_risk_score <= 69:
-                    current_risk = SeverityType.MEDIUM
-                elif current_risk_score <= 89:
-                    current_risk = SeverityType.HIGH
-                else:
-                    current_risk = SeverityType.CRITICAL
-
-                indicators.append(
-                    Indicator(
-                        value=current_indicator_value,
-                        type=current_type,
-                        severity=current_risk,
-                        comments=current_evidences.replace('"','')
-                    )
-                )
-                indicator_count += 1
-            else:
-                headers = False
-
-        return indicators, indicator_count
+        return current_type, current_value
 
     def validate(self, configuration) -> ValidationResult:
         """Validate the Plugin configuration parameters.
